9975ec091d7320394a92f12688e6b8e843353e53
Joanna Graczyk <joanna.graczyk1@gmail.com>
2020-01-20 13:58:48 +0100

    models/ User schema, users expres-validation

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API!' })
 );
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
