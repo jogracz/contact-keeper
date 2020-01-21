5d41036d9d5aed55444bc4713657ebeaaad98e3f
Joanna Graczyk <joanna.graczyk1@gmail.com>
2020-01-21 19:26:57 +0100

    Authentication done

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,19 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 
 // @route     GET api/auth
 // @desc      Get logged i user
 // @access    Private
-router.get('/', (req, res) => {
-  res.send('Get logged i user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     POST api/auth
