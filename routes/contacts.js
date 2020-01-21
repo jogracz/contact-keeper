bccf1a0a88e625396e58f1c281ab890457bbc614
Joanna Graczyk <joanna.graczyk1@gmail.com>
2020-01-21 20:09:48 +0100

    Get all contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Contact = require('../models/Contact');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const auth = require('../middleware/auth');
 
 // @route     GET api/contacts
 // @desc      Show all contacts
 // @access    Private
-router.get('/', (req, res) => {
-  res.send('Show all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     POST api/contacts
