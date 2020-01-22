77a580845a59c328b88b1c468c58f8c1ac55b415
Joanna Graczyk <joanna.graczyk1@gmail.com>
2020-01-22 15:11:16 +0100

    New favicon, Navbar, Home and About components

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p className='my-1'>A fullstack app for keeping contacts</p>
+      <p className='bg-dark p'>
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
